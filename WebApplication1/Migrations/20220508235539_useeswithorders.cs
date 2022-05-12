7bb6f3e15bd9849d5ff255057ff7b8888996c19f
OmarMohamed74 <omar.mohamed.abdelraouf3@gmail.com>
2022-05-12 19:32:47 +0200

    TryToFixingMaster

--- WebApplication1/Migrations/20220508235539_useeswithorders.cs
+++ WebApplication1/Migrations/20220508235539_useeswithorders.cs
@@ -10,7 +10,7 @@ namespace Ecommerce.Migrations
         {
             var sql = @"
                  CREATE VIEW View_UsersWithorder AS
-           SELECT [OrderHeaders]. [Id]
+             SELECT [OrderHeaders]. [Id]
       ,[OrderHeaders].[FirstName]
       ,[OrderHeaders].[LastName]
       ,[OrderDateTime]
@@ -18,11 +18,12 @@ namespace Ecommerce.Migrations
       ,[OrderHeaders].[Address]
       ,[Note]
       ,[BillingAddress]
-      ,p.Method ,OD.status
+      ,p.Method,C.Name As CouponName,C.Percentage ,C.ValidFrom,C.ValidTo
   FROM [EcommerceM].[dbo].[OrderHeaders]
   inner join AspNetUsers ANU on ANU.Id= [OrderHeaders].ApplicationUserId
   inner join OrderDetials OD on Od.OrderHeaderId=[OrderHeaders].Id 
-  inner join Payments P on P.Id=OD.PaymentId";
+  inner join Payments P on P.Id=OD.PaymentId
+  inner join Coupons C on C.Id=OD.CouponId";
             migrationBuilder.Sql(sql);
         }
 
