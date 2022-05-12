a8f46494edd28b64b315c321a1bedf7467ff3a30
_manalelsayed <manal.afifi626@gmail.com>
2022-05-12 07:14:17 +0200

    Forms Validation

--- WebApplication1/wwwroot/js/validate.js
+++ WebApplication1/wwwroot/js/validate.js
@@ -1,8 +1,25 @@
+//Phone Regex Validator
 jQuery.validator.addMethod("matches", function (phone_number, element) {
     phone_number = phone_number.replace(/\s+/g, "");
     return this.optional(element) || phone_number.length > 9 &&
         phone_number.match(/^0\d{10}$/);
 }, "Please specify a valid phone number");
+
+//Name Regex Validator
+jQuery.validator.addMethod("Namematch", function (Name, element) {
+    Name = Name.replace(/\s+/g, "");
+    return this.optional(element) || Name.length > 1 &&
+        Name.match(/^[a-zA-Z .'-]+$/);
+}, "Please specify a valid Name");
+
+//Password Regex Validator
+jQuery.validator.addMethod("Passmatch", function (Password, element) {
+    Password = Password.replace(/\s+/g, "");
+    return this.optional(element) || Password.length > 5 &&
+        Password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#/\$%\^&\*])(?=.{6,})/);
+}, "Please specify a valid  Password");
+
+//Order Validation
 $(function () {
     var $headerForm = $("#Orderheader");
     if ($headerForm.length) {
@@ -34,9 +51,9 @@ $(function () {
         })
     }
 });
-//$('#billingAddressCheck').click(function () {
-//    $('#billingAddress')[this.checked ? "show" : "hide"]();
-//});
+
+
+//Billing Address
 $("#billingAddressDiv").hide();
 $('input[name=billingAddressCheck]').on('change', function () {
     if ($(this).is(":checked")) {
@@ -49,53 +66,134 @@ $('input[name=billingAddressCheck]').on('change', function () {
 });
 
 ////Regestriation
-//$(function () {
-//    var $registerForm = $("#registerForm");
-//    if ($registerForm.length) {
-//        $registerForm.validate({
+$(function () {
+    var $registerForm = $("#registerForm");
+    if ($registerForm.length) {
+        $registerForm.validate({
 
-//            rules: {
-//                Phone: {
-//                    required: true,
-//                    matches: true, // <-- no such method called "matches"!
-//                    minlength: 11,
-//                    maxlength: 11
-//                },
-//                FirstName: {
-//                    required: true,
-//                    number: false
-//                },
-//                LastName: {
-//                    required: true,
-//                    number: false
-//                },
-//                Email: {
-//                    required: true,
-//                    email:true
-//                }
-//            },
-//            messages: {
-//                Phone: {
-//                    required: "Required",
-//                    matches: "ex:01xxxxxxxxx",  // <-- no such method called "matches"!
-//                    minlength: "not valid",
-//                    maxlength: "not valid"
-//                },
-//                FirstName: {
-//                    required: "Required",
-//                    number: "Please enter a valid name"
-//                },
-//                LastName: {
-//                    required: "Required",
-//                    number: "Please enter a valid name"
+            rules: {
+                "Input.PhoneNumber": {
+                    required: true,
+                    matches: true, // <-- no such method called "matches"!
+                    minlength: 11,
+                    maxlength: 11
+                },
+                "Input.FirstName": {
+                    required: true,
+                    Namematch: true
+                },
+                "Input.LastName": {
+                    required: true,
+                    Namematch: true
+                },
+                "Input.Email": {
+                    required: true,
+                    email:true
+                },
+                "Input.Password": {
+                    required: true,
+                    Passmatch: true
+                }
+            },
+            messages: {
+                "Input.PhoneNumber": {
+                    required: "Required",
+                    matches: "ex:01xxxxxxxxx",  // <-- no such method called "matches"!
+                    minlength: "not valid",
+                    maxlength: "not valid"
+                },
+                "Input.FirstName": {
+                    required: "Required",
+                    number: "Please enter a valid name"
+                },
+                "Input.LastName": {
+                    required: "Required",
+                    number: "Please enter a valid name"
 
-//                },
-//                Email: {
-//                    required: "Required",
-//                    email: "Please enter valid email"
-//                }
-//            }
+                },
+                "Input.Email": {
+                    required: "Required",
+                    email: "Please enter valid email"
+                },
+                "Input.Password": {
+                    required: "Required",
+                    Passmatch: "Password must have at least 6 characters, 1 number, 1 upper and 1 lowercase, one special chars"
+                },
+            }
 
-//        })
-//    }
-//});
+        })
+    }
+});
+
+
+//Vendor Request
+$(function () {
+    var VendorRequest = $("#form1");
+    if (VendorRequest.length) {
+        VendorRequest.validate({
+
+            rules: {
+                Name: {
+                    required: true,
+                    minlength: 3,
+                    maxlength:20
+                },
+                Description: {
+                    required: true,
+                    minlength: 3,
+                    maxlength: 20
+                },
+                Price: {
+                    required: true,
+                    number:true
+                },
+                Size: {
+                    required: true,
+                },
+            },
+            messages: {
+                Name: {
+                    required: "Required",
+                    minlength: "Minimum length: 3 charachters",
+                    maxlength: "Maximum length: 20 charachters"
+                },
+                Description: {
+                    required: "Required",
+                    minlength: "Minimum length: 3 charachters",
+                    maxlength: "Maximum length: 20 charachters"
+                },
+                Price: {
+                    required: "Required",
+                    number: "Please enter the proce Correctly"
+                },
+                Size: {
+                    required: "Required",
+                }
+            }
+
+        })
+    }
+});
+
+////categories Form
+$(function () {
+    var CategoriesForm = $("#CategoriesForm");
+    if (CategoriesForm.length) {
+        CategoriesForm.validate({
+
+            rules: {
+                CategoryName: {
+                    required: true
+                }
+            },
+            messages: {
+                CategoryName: {
+                    required: "Required"
+                }
+
+            }
+        })
+        }
+    });
+
+
