4297a74ddbafa840f336a9304a0b8c1e266fa830
esraa3bdelmonem <esraaabdelmonem0@gmail.com>
2022-05-21 23:53:25 +0200

    localization part 2

---
+++ WebApplication1/Controllers/LanguageController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace ClickPick.Controllers
+{
+    public class LanguageController : Controller
+    {
+       
+        [Route("api/[Controller]")]
+        [ApiController]
+        public class languageController : ControllerBase
+        {
+            private readonly IStringLocalizer<languageController> _localizer;
+            public languageController(IStringLocalizer<languageController> localizer)
+            {
+                _localizer = localizer;
+            }
+            [HttpGet]
+            public IActionResult Get()
+            {
+
+
+                return Ok(_localizer["welcome"].Value);
+            }
+
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -6,8 +6,12 @@ using Ecommerce.Models.Repositories;
 using Ecommerce.Models.Repositories.UnitOfWork;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Stripe;
+using System.Globalization;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -64,7 +68,21 @@ builder.Services.AddSession(
         options.Cookie.HttpOnly = true;
         options.Cookie.IsEssential = true;
     });
-
+//AddLocalization
+builder.Services.AddLocalization(options => { options.ResourcesPath = "Resources"; });
+builder.Services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix).AddDataAnnotationsLocalization();
+builder.Services.Configure<RequestLocalizationOptions>(
+    opt =>
+    {
+        var supportCultures = new List<CultureInfo>
+        {
+            new CultureInfo("en"),
+            new CultureInfo("ar-EG")
+        };
+        opt.DefaultRequestCulture = new RequestCulture(culture: "en", uiCulture: "en");
+        opt.SupportedCultures = supportCultures;
+        opt.SupportedUICultures = supportCultures;
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -83,7 +101,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+var locoptions = app.Services.GetService<IOptions<RequestLocalizationOptions>>();
+app.UseRequestLocalization(locoptions.Value);
 
 StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
 
